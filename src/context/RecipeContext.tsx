139b69b831bf3ecd77dd0d43ed7a59b6db1146c6
Michael Takeuchi <mikemtaki@gmail.com>
2022-07-21 16:32:13 -0700

    fetch random meals

--- src/context/RecipeContext.tsx
+++ src/context/RecipeContext.tsx
@@ -1,10 +1,21 @@
-import { createContext, useState } from "react";
-import {IRecipeContext, IRecipe} from '../recipe.model'
+import { createContext, useState, useEffect} from "react";
+import {IRecipeContext, IRecipe, IRecipeData} from '../recipe.model'
+import { fetchRandomRecipes } from "../utils/api";
 
 const RecipeContext = createContext<IRecipeContext | undefined>(undefined);
 
-export const RecipeProvider = ({children} :any) => {
+export const RecipeProvider = ({children}: any) => {
     const [recipes, setRecipes] = useState<IRecipe[]>([]);
+
+    useEffect(() => {
+      const getRecipes = () => {
+        fetchRandomRecipes().then((data: any) => setRecipes(data))
+      }
+
+      getRecipes()
+      }, [])
+
+      console.log(recipes)
 
     return (
         <RecipeContext.Provider value={{recipes, setRecipes}}>{children}</RecipeContext.Provider>
---
+++ src/utils/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://www.themealdb.com/api/json/v1/1'
+
+export const fetchRandomRecipes = async () => {
+    try {
+      const meals = [];
+      for (let i = 0; i < 4; i++) {
+        meals.push(
+          await axios
+            .get(`${BASE_URL}/random.php`)
+            .then((response) => {
+                return {recipe: {...response.data.meals[0]}, liked: false}
+            })
+            .catch((error) => console.log(error))
+        );
+      }
+      if (meals) {
+        return meals;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };

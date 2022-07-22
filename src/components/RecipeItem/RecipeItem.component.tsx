c96e5a6e4c032ad5ff3f3a36bfa6b4c56747a435
Michael Takeuchi <mikemtaki@gmail.com>
2022-07-21 18:33:39 -0700

    style recipe items

--- src/components/RecipeItem/RecipeItem.component.tsx
+++ src/components/RecipeItem/RecipeItem.component.tsx
@@ -6,7 +6,8 @@ const RecipeItem = ({recipe}: IRecipeItem) => {
     console.log(recipe)
   return (
     <li key={recipe.idMeal} className="recipe-item">
-        <p>{recipe.strMeal}</p>
+        <img className="recipe-img" src={recipe.strMealThumb} alt={recipe.strMeal}/>
+        <p className="recipe-name">{recipe.strMeal}</p>
     </li>
   )
 }
